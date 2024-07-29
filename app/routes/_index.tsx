6f50634fea249c58c2feca854295136f46904a49
Derek Scruggs <me@derekscruggs.com>
2024-07-29 14:16:47 -0500

    added meta preview

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,21 @@ export const loader: LoaderFunction = async (args: LoaderFunctionArgs) => {
 
 export const meta: MetaFunction = () => [
   { title: 'Trybe' },
-  { name: 'description', content: 'Build new habits. Join challenges. Meet your Trybe.' }
+  { name: 'description', content: 'Build new habits. Join challenges. Meet your Trybe.' },
+  { name: 'og:image', content: '/images/trybe-preview.png' },
+  { name: 'twitter:image', content: '/images/trybe-preview.png' },
+  { property: 'og:url', content: 'https://app.jointhetrybe.com' },
+  { property: 'og:type', content: 'website' },
+  { property: 'og:title', content: 'Trybe' },
+  { property: 'og:description', content: 'Build new habits. Join challenges. Meet your Trybe.' },
+  { property: 'og:image', content: '' },
+  { name: 'twitter:card', content: 'summary_large_image' },
+  { name: 'twitter:domain', content: 'app.jointhetrybe.com' },
+  { name: 'twitter:url', content: 'https://app.jointhetrybe.com' },
+  { name: 'twitter:title', content: 'Trybe' },
+  { name: 'twitter:description', content: 'Build new habits. Join challenges. Meet your Trybe.' },
+  { name: 'twitter:image', content: '/images/trybe-preview.png' }
+
 ]
 export default function Index (): JSX.Element {
   return (
