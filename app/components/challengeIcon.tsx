f800812429384b01a85b4bb16c676ccd3cecf009
Derek Scruggs <me@derekscruggs.com>
2024-08-08 09:39:29 -0500

    changed icons to object with width instead of array

--- app/components/challengeIcon.tsx
+++ app/components/challengeIcon.tsx
@@ -1,28 +1,32 @@
 import { HiOutlineQuestionMarkCircle } from 'react-icons/hi'
 
-export const iconFiles = [
-  'YogaLight.png',
-  'YogaDark.png',
-  'WorkoutDark.png',
-  'SelfCareHeart.png',
-  'RunnerPink.png',
-  'MuscleBrain.png',
-  'MeditatorMedium.png',
-  'MeditatorLight.png',
-  'MeditatorDark.png',
-  'JournalPink.png',
-  'JournalDark.png',
-  'EnvironmentMedium.png'
-]
+export const iconFiles: Record<string, number> = {
+  'YogaLight.png': 150,
+  'YogaDark.png': 150,
+  'WorkoutDark.png': 150,
+  'SelfCareHeart.png': 150,
+  'RunnerPink.png': 150,
+  'MuscleBrain.png': 150,
+  'MeditatorMedium.png': 150,
+  'MeditatorLight.png': 150,
+  'MeditatorDark.png': 150,
+  'JournalPink.png': 150,
+  'JournalDark.png': 150,
+  'EnvironmentMedium.png': 150
+}
 interface ChallengeIconProps {
   icon?: string
 }
 const ChallengeIcon = ({ icon }: ChallengeIconProps): JSX.Element => {
+  let width = 150
+  if (icon && iconFiles[icon]) {
+    width = iconFiles[icon]
+  }
   return (
     <>
     {icon && icon.includes('png')
       ? (
-        <img src={`/images/icons/${icon}`} width="150" className="cursor-pointer" />
+        <img src={`/images/icons/${icon}`} width={width} className="cursor-pointer" />
         )
       : (
       <HiOutlineQuestionMarkCircle className="w-24 h-24 text-grey" />
