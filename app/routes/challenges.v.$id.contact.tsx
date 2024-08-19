35e263c095f93816b3c443a75c6799ad430de556
Derek Scruggs <me@derekscruggs.com>
2024-08-19 08:48:01 -0500

    fix to schedule conditional

---
+++ app/routes/challenges.v.$id.contact.tsx
@@ -0,0 +1,53 @@
+import { useLoaderData, useRouteLoaderData, Link } from '@remix-run/react'
+import { useContext } from 'react'
+import { requireCurrentUser } from '~/models/auth.server'
+import { CurrentUserContext } from '~/utils/CurrentUserContext'
+import { type Post } from '@prisma/client'
+import { type LoaderFunction, type LoaderFunctionArgs } from '@remix-run/node'
+import { prisma } from '~/models/prisma.server'
+import { type Challenge } from '~/utils/types'
+import ChallengeSchedule from '~/components/challengeSchedule'
+
+interface ChallengeScheduleData {
+  posts: Post[]
+}
+export const loader: LoaderFunction = async (args: LoaderFunctionArgs): Promise<ChallengeScheduleData> => {
+  await requireCurrentUser(args)
+  const { params } = args
+  const posts = await prisma.post.findMany({
+    where: {
+      challengeId: Number(params.id)
+    },
+    orderBy: [
+      { publishAt: 'asc' },
+      { createdAt: 'asc' }
+    ]
+  })
+  const data: ChallengeScheduleData = {
+    posts: posts.map(post => ({
+      ...post,
+      createdAt: new Date(post.createdAt),
+      publishAt: post.publishAt ? new Date(post.publishAt) : null
+    }))
+  }
+  return data
+}
+export default function Program (): JSX.Element {
+  const { challenge } = useRouteLoaderData<typeof useRouteLoaderData>('routes/challenges.v.$id') as { challenge: Challenge }
+  const { posts } = useLoaderData<typeof loader>() as ChallengeScheduleData
+  const { currentUser } = useContext(CurrentUserContext)
+  return (
+    <>
+    {0 > 1 &&
+    <>
+        foo
+        {challenge.userId === currentUser?.id
+          ? <div>You have not scheduled content. <Link to={`/challenges/v/${challenge.id}/schedule`}>Edit schedule.</Link></div>
+          : <div>Schedule has not been published yet. </div>
+        }
+      </>
+      }
+      <ChallengeSchedule challenge={challenge} posts={posts} key={challenge.id} isSchedule={false} />
+    </>
+  )
+}
