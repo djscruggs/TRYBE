992eaad44bec30ce6d6b79f9532b1683dc634f59
Derek Scruggs <me@derekscruggs.com>
2024-09-09 16:22:19 -0500

    removed top & bottom bars

--- app/routes/challenges.tsx
+++ app/routes/challenges.tsx
@@ -21,10 +21,10 @@ export default function ChallengesIndex (): JSX.Element {
   return (
     <div className='flex items-center  max-w-xl mt-14'>
       <div className="flex flex-col items-center max-w-lg w-full">
-        <h1 className="text-3xl font-bold mb-4 w-full">
+        <h1 className="text-3xl font-bold mb-4 w-full ml-2 md:ml-0">
               Challenges
         </h1>
-        <p className="rounded-md p-4 bg-yellow">View your current challenges, browse upcoming challenges, or start your own!</p>
+        <p className='ml-2 md:ml-0'>View your current challenges, browse upcoming challenges, or start your own!</p>
           {currentUser && <Button placeholder='Create a Challenge' size="sm" onClick={() => { navigate('./new') }} className="bg-red mb-4 mt-4">Create a Challenge</Button>}
           <Outlet />
       </div>
