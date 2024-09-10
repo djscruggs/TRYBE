32ad22ce79fa50ff624a04a53c8bf8bf955fda79
Derek Scruggs <me@derekscruggs.com>
2024-09-10 11:55:04 -0500

    fixed scroll into view for chat after new comment

--- app/components/chatContainer.tsx
+++ app/components/chatContainer.tsx
@@ -14,16 +14,27 @@ export default function ChatContainer (props: CommentsProps): JSX.Element {
   useEffect(() => {
     setLikedCommentIds(props.likedCommentIds)
   }, [props.likedCommentIds])
+  console.log(comments)
+  const uniqueComments = (): Comment[] => {
+    const unique = Array.from(new Set(comments.map(comment => comment.id)))
+      .map(id => comments.find(comment => comment.id === id))
+      .sort((a, b) => (new Date(a?.createdAt ?? 0).getTime()) - (new Date(b?.createdAt ?? 0).getTime()))
+    if (!unique) {
+      return []
+    }
+    return unique as Comment[]
+  }
   return (
     <div className='w-full' id='comments'>
-      {firstComment &&
-        <ChatItem key={`comment-${firstComment.id}`} comment={firstComment} likedCommentIds={likedCommentIds} />
-      }
-      {comments.map((comment) => {
+
+      {uniqueComments().map((comment) => {
         return (
           <ChatItem key={`comment-${comment.id}`} comment={comment} likedCommentIds={likedCommentIds} />
         )
       })}
+      {firstComment &&
+        <ChatItem key={`comment-${firstComment.id}`} comment={firstComment} likedCommentIds={likedCommentIds} />
+      }
     </div>
   )
 }
