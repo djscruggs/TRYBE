ce9d4752d6fb0f8078ddff18670f9d4479a2cec4
Derek Scruggs <me@derekscruggs.com>
2024-10-05 11:27:57 -0500

    fixed checkedInToday to accomdate user time zone

--- app/utils/helpers.tsx
+++ app/utils/helpers.tsx
@@ -19,6 +19,14 @@ export const copyToClipboard = async (text: string): Promise<void> => {
     console.error(error)
   }
 }
+export const convertToLocalDateString = (user: CurrentUser | null, date: Date): string => {
+  if (user?.locale) {
+    const locale = user.locale
+    return date.toLocaleDateString(locale, { timeZone: 'UTC' })
+  }
+  return date.toISOString().split('T')[0]
+}
+
 // helper  function that converts booleans, integers and dates from strings to the proper type
 export function convertStringValues (obj: any): any {
   const result: any = {}
