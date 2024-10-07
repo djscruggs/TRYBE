1a6eae452303232fd53700f49d02f2438d9fac24
Derek Scruggs <me@derekscruggs.com>
2024-10-07 15:25:58 -0500

    fix to youtube video on mobile

--- app/components/linkRenderer.tsx
+++ app/components/linkRenderer.tsx
@@ -16,15 +16,16 @@ const LinkRenderer: React.FC<LinkRendererProps> = ({ text }) => {
   if (matchYouTube) {
     const videoId = matchYouTube[1]
     return (
-      <iframe
-        width="500"
-        height="280"
-        src={`https://www.youtube.com/embed/${videoId}`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <div className="aspect-w-16 aspect-h-9">
+        <iframe
+          className="w-full h-full"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     )
   }
 
