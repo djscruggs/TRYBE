35d5f78978975b3c8089249a4929d648e0457e67
Derek Scruggs <me@derekscruggs.com>
2024-11-19 13:00:34 -0600

    added upcoming challenges to main view

--- app/routes/challenges.$status.tsx
+++ app/routes/challenges.$status.tsx
@@ -4,10 +4,12 @@ import type { ChallengeSummary, MemberChallenge } from '~/utils/types'
 import ChallengeList from '~/components/challengeList'
 import axios from 'axios'
 import { CurrentUserContext } from '~/utils/CurrentUserContext'
+import { Switch } from '@material-tailwind/react'
 export default function ChallengesIndex (): JSX.Element {
   const [myChallenges, setMyChallenges] = useState<ChallengeSummary[]>([])
   const [challenges, setChallenges] = useState<ChallengeSummary[]>([])
   const [category, setCategory] = useState<string>('')
+  const [upcomingChallenges, setUpcomingChallenges] = useState<ChallengeSummary[]>([])
   const params = useParams()
   const [status, setStatus] = useState(params.status ?? 'active')
   const [loading, setLoading] = useState(true)
@@ -46,8 +48,13 @@ export default function ChallengesIndex (): JSX.Element {
     setChallenges(otherChallenges)
     setLoading(false)
   }
+  const loadUpcomingChallenges = async (): Promise<void> => {
+    const response = await axios.get('/api/challenges/upcoming')
+    setUpcomingChallenges(response.data.challenges)
+  }
   useEffect(() => {
     void loadData()
+    void loadUpcomingChallenges()
   }, [status])
 
   return (
@@ -63,18 +70,19 @@ export default function ChallengesIndex (): JSX.Element {
             }
             <ChallengeList challenges={myChallenges} memberships={memberships} isLoading={loading} />
           </div>
-          {!loading && challenges.length > 0 &&
+          {!loading && upcomingChallenges.length > 0 &&
             <>
               <div className='text-red'>Browse Challenges</div>
-              <div className='text-lg py-2 flex items-center justify-start w-full relative'>
-                <div className={`w-fit cursor-pointer ${category === 'meditation' ? 'border-b-2 border-red' : ''}`} onClick={() => { handleCategoryChange('meditation') }}>Meditation</div>
-                <div className={`w-fit mx-8 cursor-pointer ${category === 'journal' ? 'border-b-2 border-red' : ''}`} onClick={() => { handleCategoryChange('journal') }}>Journal</div>
-                <div className={`w-fit mr-8 cursor-pointer ${category === 'creativity' ? 'border-b-2 border-red' : ''}`} onClick={() => { handleCategoryChange('creativity') }}>Creativity</div>
-                <div className={`w-fit mr-8 cursor-pointer ${category === 'self-guided' ? 'border-b-2 border-red' : ''}`} onClick={() => { handleCategoryChange('self-guided') }}>Self-Guided</div>
+              <div className='py-2 space-x-2 flex items-center justify-between md:justify-start w-full relative text-white text-sm'>
+                <div className={`w-fit p-1 px-2  rounded-md cursor-pointer ${category === 'meditation' ? 'bg-gray-400' : 'text-black bg-gray-100'}`} onClick={() => { handleCategoryChange('meditation') }}>Meditation</div>
+                <div className={`w-fit p-1 px-2  rounded-md cursor-pointer ${category === 'journal' ? 'bg-gray-400' : 'text-black bg-gray-100'}`} onClick={() => { handleCategoryChange('journal') }}>Journal</div>
+                <div className={`w-fit p-1 px-2  rounded-md cursor-pointer ${category === 'creativity' ? 'bg-gray-400' : 'text-black bg-gray-100'}`} onClick={() => { handleCategoryChange('creativity') }}>Creativity</div>
+                <div className='w-fit mx-2 text-grey'> | </div>
+                <div className={`w-fit p-1 px-2 rounded-md cursor-pointer ${category === 'self-guided' ? 'bg-gray-400' : 'text-black bg-gray-100'}`} onClick={() => { handleCategoryChange('self-guided') }}>Self-Guided</div>
+                <Switch crossOrigin="anonymous" />
               </div>
               <div className="flex flex-col items-center max-w-lg w-full">
-                <div className="text-center mt-10">No {status !== 'mine' ? status : ''} challenges found</div>
-                <ChallengeList challenges={challenges} memberships={memberships} isLoading={loading} />
+                <ChallengeList challenges={upcomingChallenges} memberships={memberships} isLoading={loading} />
               </div>
             </>
           }
