b5afeece57c92d4937ca51aa92e640986070f47b
Derek Scruggs <me@derekscruggs.com>
2024-12-21 09:22:58 -0600

    refactored my checkins to use api loader

---
+++ app/routes/api.checkins.$challengeId.($userId).ts
@@ -0,0 +1,14 @@
+import { requireCurrentUser } from '~/models/auth.server'
+import { type LoaderFunction } from '@remix-run/node'
+import { fetchCheckIns } from '~/models/challenge.server'
+import { type CheckIn } from '~/utils/types'
+interface LoaderData {
+  checkIns: CheckIn[]
+}
+export const loader: LoaderFunction = async (args): Promise<LoaderData> => {
+  await requireCurrentUser(args)
+  const userId = Number(args.params.userId ?? undefined)
+  const challengeId = Number(args.params.challengeId)
+  const checkIns = await fetchCheckIns({ userId, challengeId })
+  return { checkIns }
+}
--- app/routes/challenges.v.$id.checkins.tsx
+++ app/routes/challenges.v.$id.checkins.tsx
@@ -1,23 +1,15 @@
-import { requireCurrentUser } from '~/models/auth.server'
-import { type LoaderFunction, type MetaFunction, json } from '@remix-run/node'
-import { useLoaderData, useRouteLoaderData } from '@remix-run/react'
-import { fetchCheckIns } from '~/models/challenge.server'
-import type { Challenge, MemberChallenge } from '~/utils/types'
+import { type MetaFunction, useRouteLoaderData } from '@remix-run/react'
+import type { Challenge, CheckIn, MemberChallenge } from '~/utils/types'
 import CheckinsList from '~/components/checkinsList'
 import { differenceInDays, format } from 'date-fns'
 import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { CurrentUserContext } from '~/utils/CurrentUserContext'
 import { CheckInButton } from '~/components/checkinButton'
-
+import axios from 'axios'
+import { Spinner } from '@material-tailwind/react'
 import 'react-circular-progressbar/dist/styles.css'
-export const loader: LoaderFunction = async (args) => {
-  const currentUser = await requireCurrentUser(args)
-  const userId = Number(args.params.userId ?? currentUser?.id)
-  const challengeId = Number(args.params.id)
-  const checkIns = await fetchCheckIns({ userId, challengeId }) as { error?: string }
-  return json({ checkIns })
-}
+
 export const meta: MetaFunction = () => {
   return [
     { title: 'Checkins' },
@@ -28,42 +20,54 @@ export const meta: MetaFunction = () => {
   ]
 }
 export default function MyCheckIns (): JSX.Element {
-  const { checkIns, error } = useLoaderData<typeof loader>()
   const { membership, challenge } = useRouteLoaderData<typeof useRouteLoaderData>('routes/challenges.v.$id') as { membership: MemberChallenge, challenge: Challenge }
   const { currentUser } = useContext(CurrentUserContext)
-  if (error) {
-    return <h1>{error}</h1>
-  }
+  const [checkIns, setCheckIns] = useState<CheckIn[]>([])
+  const [isLoading, setIsLoading] = useState(true)
   if (!membership && challenge.userId !== currentUser?.id) {
     return <></>
   }
-
+  const fetchCheckIns = async (): Promise<void> => {
+    setIsLoading(true)
+    const response = await axios.get(`/api/checkins/${challenge.id}/${currentUser?.id}`)
+    setCheckIns(response.data.checkIns as CheckIn[])
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    void fetchCheckIns()
+  }, [challenge.id, currentUser?.id])
   return (
-    <div className='flex flex-col items-start justify-center mt-4  w-full max-w-lg md:max-w-xl'>
-      <div className='w-full flex items-center justify-center mb-8'>
-        <div className='max-w-[200px] flex-col items-center justify-center'>
+    <div className={`flex flex-col ${isLoading ? 'items-center' : 'items-start'} justify-center mt-4  w-full max-w-lg md:max-w-xl`}>
+      {isLoading
+        ? <Spinner />
+        : <>
+        <div className='w-full flex items-center justify-center mb-8'>
+          <div className='max-w-[200px] flex-col items-center justify-center'>
             <ChallengeMemberProgressChart challenge={challenge} checkIns={checkIns} />
             <div className='mt-4 flex items-center justify-center'>
-              <CheckInButton challenge={challenge} membership={membership} />
+              <CheckInButton challenge={challenge} membership={membership} afterCheckIn={fetchCheckIns} />
             </div>
+          </div>
         </div>
-
-      </div>
-      <CheckinsList checkIns={checkIns} allowComments={false} posts={[]} newestComment={null}/>
+        <CheckinsList checkIns={checkIns} allowComments={false} posts={[]} newestComment={null}/>
+      </>
+      }
     </div>
   )
 }
 
-export function ChallengeMemberProgressChart ({ challenge, checkIns }: { challenge: Challenge, checkIns: Array<{ createdAt: string }> }): JSX.Element {
-  const numDays = (challenge?.endAt && challenge.startAt) ? differenceInDays(challenge.endAt, challenge.startAt) : 0
+export function ChallengeMemberProgressChart ({ challenge, checkIns }: { challenge: Challenge, checkIns: CheckIn[] }): JSX.Element {
+  const numDays = challenge.type === 'SELF_LED'
+    ? challenge.numDays
+    : (challenge?.endAt && challenge.startAt) ? differenceInDays(challenge.endAt, challenge.startAt) : 0
   const typedCheckIns = checkIns as Array<{ createdAt: string }>
   const uniqueDays = new Set(typedCheckIns.map(checkIn => format(new Date(checkIn.createdAt), 'yyyy-MM-dd'))).size
-  const progress = (uniqueDays / numDays) * 100
+  const progress = numDays ? (uniqueDays / numDays) * 100 : 0
   return (
     <div className='flex flex-col items-center justify-center'>
     <CircularProgressbarWithChildren
       value={progress}
-      maxValue={numDays}
+      maxValue={numDays ?? 0}
 
       strokeWidth={5}
       styles={buildStyles({
