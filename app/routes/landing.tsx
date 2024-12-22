a4889fb48af4827dead5e5c21db9522a4c79f040
Derek Scruggs <me@derekscruggs.com>
2024-12-21 19:38:20 -0600

    added landing route for testing

---
+++ app/routes/landing.tsx
@@ -0,0 +1,25 @@
+import LandingPage from '~/components/landingPage'
+
+export const meta: MetaFunction = () => [
+  { title: 'Trybe' },
+  { name: 'description', content: 'Build new habits. Join challenges. Meet your Trybe.' },
+  { property: 'og:url', content: 'https://app.jointhetrybe.com' },
+  { property: 'og:type', content: 'website' },
+  { property: 'og:title', content: 'Trybe' },
+  { property: 'og:description', content: 'Build new habits. Join challenges. Meet your Trybe.' },
+  { property: 'og:image', content: '/images/trybe-preview.png' },
+  { name: 'twitter:card', content: 'summary_large_image' },
+  { name: 'twitter:domain', content: 'app.jointhetrybe.com' },
+  { name: 'twitter:url', content: 'https://app.jointhetrybe.com' },
+  { name: 'twitter:title', content: 'Trybe' },
+  { name: 'twitter:description', content: 'Build new habits. Join challenges. Meet your Trybe.' },
+  { name: 'twitter:image', content: '/images/trybe-preview.png' }
+
+]
+export default function Index (): JSX.Element {
+  return (
+          <>
+            <LandingPage />
+          </>
+  )
+}
