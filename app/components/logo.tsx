ce7208fbfa855c837b43b428c29558f591e117d0
Derek Scruggs <me@derekscruggs.com>
2024-12-23 11:15:39 -0600

    added explanatory info on signup page

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -3,3 +3,18 @@ export default function Logo ({ size = '24px', backgroundColor = 'white', classN
         <img src="/logo.png" className={`h-[${size}] bg-${backgroundColor} rounded-full ${className}`} />
   )
 }
+// not used, this is used to generate tailwind classes
+const sizes = {
+  24: <span className='h-[24px] w-[24px]'>T</span>,
+  36: <span className='h-[36px] w-[36px]'>T</span>,
+  40: <span className='h-[40px] w-[40px]'>T</span>,
+  48: <span className='h-[48px] w-[48px]'>T</span>,
+  60: <span className='h-[60px] w-[60px]'>T</span>,
+  80: <span className='h-[80px] w-[80px]'>T</span>,
+  100: <span className='h-[100px] w-[100px]'>T</span>,
+  120: <span className='h-[120px] w-[120px]'>T</span>,
+  140: <span className='h-[140px] w-[140px]'>T</span>,
+  160: <span className='h-[160px] w-[160px]'>T</span>,
+  180: <span className='h-[180px] w-[180px]'>T</span>,
+  200: <span className='h-[200px] w-[200px]'>T</span>
+}
--- app/routes/signup.$.tsx
+++ app/routes/signup.$.tsx
@@ -1,12 +1,8 @@
-import { type LoaderFunction, type LoaderFunctionArgs, redirect } from '@remix-run/node'
-import { getAuth } from '@clerk/remix/ssr.server'
-import { Button } from '@material-tailwind/react'
+import { type LoaderFunction, type LoaderFunctionArgs } from '@remix-run/node'
+import { SignUp } from '@clerk/remix'
+import Logo from '~/components/logo'
+import { useState } from 'react'
 import { Link } from '@remix-run/react'
-import {
-  SignUp,
-  SignedIn,
-  SignedOut
-} from '@clerk/remix'
 
 export const loader: LoaderFunction = async (args: LoaderFunctionArgs) => {
   // const { userId } = await getAuth(args)
@@ -16,8 +12,11 @@ export const loader: LoaderFunction = async (args: LoaderFunctionArgs) => {
   return null
 }
 export default function SignUpPage (): JSX.Element {
+  const [showSignup, setShowSignup] = useState(false)
   return (
     <div className="justify-center items-center flex flex-col gap-y-4 w-screen h-screen relative">
+      {!showSignup && <SignupNotice onSignUp={() => { setShowSignup(true) }} />}
+      {showSignup &&
         <SignUp
         appearance={{
           variables: {
@@ -30,6 +29,24 @@ export default function SignUpPage (): JSX.Element {
           }
         }}
         />
+      }
+
+    </div>
+  )
+}
+
+function SignupNotice ({ onSignUp }: { onSignUp: () => void }): JSX.Element {
+  return (
+    <div className='flex flex-col items-center justify-center '>
+      <div className='text-xl text-red'>WELCOME TO</div>
+      <div className='text-8xl font-bold text-red text-center w-full font-cursive'>Trybe</div>
+      <div className='flex mt-6'>
+        <Logo size='180px' backgroundColor='white' />
+      </div>
+      <p className='my-6 text-center text-lg w-3/4 md:w-1/2 text-[#696262]'>In order to join a challenge or created your own, an active Trybe account is required.</p>
+      <button className='bg-red text-white text-xl px-4 py-2 w-3/4 md:w-1/2 rounded-full shadow-md' onClick={onSignUp}>Sign up</button>
+      <p className='mt-6 mb-4 text-center text-xl w-3/4 md:w-1/2 text-grey'>Already have an account? </p>
+      <Link to='/login' className='text-red text-xl underline'>Log In</Link>
 
     </div>
   )
