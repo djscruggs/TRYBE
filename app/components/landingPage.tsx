a57cb1cf65537ae04037cf71fab15242ab1641f9
Derek Scruggs <me@derekscruggs.com>
2024-12-24 07:02:18 -0600

    added back button to top

--- app/components/landingPage.tsx
+++ app/components/landingPage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { FaArrowCircleRight } from 'react-icons/fa'
 import { useSearchParams, useNavigate } from 'react-router-dom'
+import { FaLessThan } from 'react-icons/fa6'
 import Logo from '../components/logo'
 export default function LandingPage (): JSX.Element {
   const navigate = useNavigate()
@@ -18,12 +19,18 @@ export default function LandingPage (): JSX.Element {
   }
 
   return (
-          <div className='relative w-screen max-w-[800px] h-svh items-start flex flex-col rounded-lg md:shadow-inner md:m-4  pt-4 md:pt-8 text-[#555555]'>
-            <div className='flex justify-center items-center space-x-2 w-full mb-4'>
-              <Logo size='36px' backgroundColor='white' />
+          <div className='relative w-screen max-w-[800px] border-2 border-black h-svh items-start flex-grow flex flex-col rounded-lg md:shadow-inner md:m-4  pt-4 md:pt-8 text-[#555555]'>
+            <div className='w-full mb-4'>
+              <div className='flex justify-center items-center w-1/2'>
+                <FaLessThan className={`text-lessblack text-lg text-left w-full ${step > 1 ? 'cursor-pointer' : 'invisible'}`} onClick={() => { if (step > 1) { setStep(step - 1) } }}/>
+              </div>
+
+            <div className='flex justify-center items-center w-full'>
+                <Logo size='48px' backgroundColor='white' />
+              </div>
             </div>
             {step === 1 &&
-              <div className='relative flex flex-col max-h-[800px] h-full items-start justify-start  text-red font-light max-w-[800px] w-full'>
+              <div className='relative flex flex-col h-screen flex-grow items-start justify-start  text-red font-light max-w-[800px] w-full'>
                 <div className='text-xl text-left w-full pl-20 md:pl-60 mt-10'>WELCOME TO</div>
                 <div className='text-8xl font-bold text-center w-full font-cursive'>Trybe</div>
                 <div className='text-xl text-right w-full mb-20 pr-20 md:pr-60'>(BETA)</div>
@@ -36,20 +43,22 @@ export default function LandingPage (): JSX.Element {
                   <NavigationDots step={step} />
                 </div>
                 <NavigationArrows step={step} skip={skipStep} nextStep={incrementStep} className='mt-36' />
-
               </div>
             }
             {step === 2 &&
-              <div className='relative w-full'>
-                <div className='flex-col h-[600px] w-full items-center justify-center'>
+              <div className='relative w-full h-screen'>
+                <div className='flex-col  w-full items-center justify-center'>
                   <h1 className='text-xl font-bold text-center mt-4'>What you can do with <span className='text-red font-cursive'>Trybe</span></h1>
                   <p className='text-lg text-center'>Our intentional design lets you<br /> get what you need, no matter <br />what <span className='font-bold'>#mood</span> you&apos;re in</p>
                   <div className='flex justify-center items-start  w-full mb-8'>
-                    <img src="/images/landing/process.png" alt="process" className='h-[300px] mt-4' />
+                    <img src="/images/landing/howitworks.png" alt="How it works" className='h-[400px]' />
                   </div>
-                  <NavigationDots step={step} />
-                  <NavigationArrows step={step} skip={skipStep} nextStep={incrementStep} className='mt-24' />
-                </div>
+
+                    <NavigationDots step={step} />
+                    <div className='flex-grow items-start border-2 border-red w-full absolute bottom-0'>
+                      <NavigationArrows step={step} skip={skipStep} nextStep={incrementStep} className='mt-24' />
+                    </div>
+                  </div>
 
               </div>
             }
@@ -70,7 +79,7 @@ export default function LandingPage (): JSX.Element {
                     </p>
                   </div>
                   <div className='flex justify-center items-start  w-full mb-2'>
-                    <img src="/images/landing/improvement.png" alt="improvement" className='w-auto h-[200px]' />
+                    <img src="/images/landing/improvement.png" alt="Continuous improvement is better than delayed perfection" className='w-auto h-[200px]' />
                   </div>
                   <div className='flex-col justify-center items-center space-x-2 w-full'>
                     <NavigationDots step={step} />
@@ -113,16 +122,16 @@ const NavigationArrows = ({ step = 1, nextStep, skip, className }: { step: numbe
 
   }, [step])
   return (
-    <div className={`w-full relative flex justify-center items-center space-x-2 ${className}`}>
+    <div className={`w-full relative flex justify-start items-center space-x-2 ${className}`}>
       {step < 3 &&
         <>
-        <div className='z-10 text-gray-400 cursor-pointer text-2xl absolute bottom-3 left-10 md:left-36' onClick={() => { skip() }}>SKIP</div>
-        <FaArrowCircleRight className='z-10 text-red rounded-full text-6xl cursor-pointer absolute bottom-0 right-10 md:right-36' onClick={() => { nextStep() }}/>
+        <div className='z-10 text-gray-400 cursor-pointer text-xl absolute bottom-7 left-6' onClick={() => { skip() }}>SKIP</div>
+        <FaArrowCircleRight className='z-10 text-red rounded-full text-4xl cursor-pointer absolute bottom-6 right-6' onClick={() => { nextStep() }}/>
         </>
       }
       {step > 2 &&
 
-      <button onClick={getStarted} className="p-2 py-1 rounded-full bg-red text-white text-sm uppercase hover:bg-gray-200 absolute bottom-0 right mr-4">
+      <button onClick={getStarted} className="p-2 py-1 rounded-full bg-red text-white text-sm uppercase hover:bg-gray-200 absolute bottom-0">
         Get Started
       </button>
 }
