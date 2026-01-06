e1e67d01f4944b9b2dfc99cadc1e7a8fa63a36f7
Derek Scruggs <me@derekscruggs.com>
2026-01-06 11:55:26 -0600

    linter fixes

--- app/routes/login.$.tsx
+++ app/routes/login.$.tsx
@@ -1,26 +1,26 @@
-import { JSX } from 'react'
-import { SignIn } from "@clerk/react-router";
-import { type LoaderFunctionArgs } from "react-router";
-import { rootAuthLoader } from "@clerk/react-router/server";
+import { type JSX } from 'react'
+import { SignIn } from '@clerk/react-router'
+import { type LoaderFunctionArgs, type LoaderFunction } from 'react-router'
+import { rootAuthLoader } from '@clerk/react-router/server'
 
-export async function loader(args: LoaderFunctionArgs) {
-  return rootAuthLoader(args);
+export async function loader(
+  args: LoaderFunctionArgs
+): Promise<LoaderFunction> {
+  return (await rootAuthLoader(args)) as LoaderFunction
 }
 
 export default function SignInPage(): JSX.Element {
   return (
     <div className="w-full flex flex-col justify-center items-start mt-10">
-        <SignIn
-          routing="hash"
-          fallbackRedirectUrl="/home"
-          afterSignInUrl="/home"
-          appearance={{
-            elements: {
-              formButtonPrimary: "bg-red hover:bg-yellow text-sm normal-case",
-            },
-          }}
-        />
-      
+      <SignIn
+        routing="hash"
+        fallbackRedirectUrl="/home"
+        appearance={{
+          elements: {
+            formButtonPrimary: 'bg-red hover:bg-yellow text-sm normal-case'
+          }
+        }}
+      />
     </div>
-  );
+  )
 }
--- app/ui/navlinks.tsx
+++ app/ui/navlinks.tsx
@@ -1,7 +1,7 @@
 // NavLinks.tsx
 
-import { JSX } from 'react'
-import { useLocation, useNavigate, useNavigation } from 'react-router';
+import { type JSX } from 'react'
+import { useLocation, useNavigate, useNavigation, Link } from 'react-router'
 import {
   PlusCircleIcon,
   TrophyIcon,
@@ -11,8 +11,7 @@ import {
 import { HiOutlineLogout, HiOutlineLogin } from 'react-icons/hi'
 import { CurrentUserContext } from '~/contexts/CurrentUserContext'
 import { useContext, useState } from 'react'
-import { Spinner } from '~/components/ui/spinner';
-import { Link } from 'react-router';
+import { Spinner } from '~/components/ui/spinner'
 import { useClerk } from '@clerk/react-router'
 import useGatedNavigate from '~/hooks/useGatedNavigate'
 import { useDeviceContext } from '~/contexts/DeviceContext'
@@ -26,7 +25,7 @@ const NavLinks = (): JSX.Element => {
     if (!isMobile) {
       await ClerkSignOut()
     } else {
-      navigate('/logout', {
+      void navigate('/logout', {
         state: {
           redirectUrl: '/challenges'
         }
@@ -40,63 +39,110 @@ const NavLinks = (): JSX.Element => {
     setIsNewOpen(!isNewOpen)
   }
   return (
-      <>
+    <>
+      <div className="flex flex-col justify-start items-center h-screen min-h-full ">
+        <div
+          className={`w-24 flex items-center flex-col text-darkgrey text-center mb-4 p-2 rounded-lg ${location.pathname === '/' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}
+        >
+          <Link
+            to="/home"
+            className="flex items-center flex-col"
+            prefetch="render"
+          >
+            <HomeIcon className='className="h-8 w-8 cursor-pointer mb-1' />
+            <span className="cursor-pointer ">Home</span>
+          </Link>
+        </div>
+        <div
+          className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/challenges' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}
+        >
+          <Link
+            to="/challenges"
+            className="flex items-center flex-col"
+            prefetch="render"
+          >
+            <TrophyIcon className="h-8 w-8 cursor-pointer mb-1y" />
+            <span className="cursor-pointer">Challenges</span>
+          </Link>
+        </div>
 
-        <div className="flex flex-col justify-start items-center h-screen min-h-full ">
-          <div className={`w-24 flex items-center flex-col text-darkgrey text-center mb-4 p-2 rounded-lg ${location.pathname === '/' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}>
-            <Link to="/home" className='flex items-center flex-col' prefetch='render'>
-              <HomeIcon className='className="h-8 w-8 cursor-pointer mb-1' />
-              <span className="cursor-pointer ">Home</span>
+        <div
+          className="relative"
+          onMouseEnter={toggleNewOpen}
+          onMouseLeave={toggleNewOpen}
+        >
+          <div
+            className={
+              'w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg'
+            }
+          >
+            <PlusCircleIcon
+              className="h-8 w-8 cursor-pointer mb-1y text-red"
+              onClick={() => {
+                gatedNavigate('/challenges/new', true)
+              }}
+            />
+          </div>
+        </div>
+
+        {!currentUser && (
+          <div
+            className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/challenges' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}
+          >
+            <Link
+              to="/login"
+              className="flex items-center flex-col"
+              prefetch="render"
+            >
+              <HiOutlineLogin className="h-8 w-8 cursor-pointer mb-1y" />
+              <span className="cursor-pointer">Login</span>
             </Link>
           </div>
-          <div className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/challenges' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}>
-            <Link to="/challenges" className='flex items-center flex-col' prefetch='render'>
-              <TrophyIcon className='h-8 w-8 cursor-pointer mb-1y' />
-              <span className="cursor-pointer">Challenges</span>
-            </Link>
-          </div>
+        )}
 
-          <div className='relative' onMouseEnter={toggleNewOpen} onMouseLeave={toggleNewOpen}>
-            <div className={'w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg'}>
-              <PlusCircleIcon className='h-8 w-8 cursor-pointer mb-1y text-red' onClick={() => { gatedNavigate('/challenges/new', true) }} />
-            </div>
-          </div>
-
-          {!currentUser &&
-            <div className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/challenges' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}>
-              <Link to="/login" className='flex items-center flex-col' prefetch='render'>
-                <HiOutlineLogin className='h-8 w-8 cursor-pointer mb-1y' />
-                <span className="cursor-pointer">Login</span>
+        {currentUser?.role === 'ADMIN' && (
+          <>
+            <div
+              className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/challenges/mine' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}
+            >
+              <Link
+                to="/challenges/mine"
+                className="flex items-center flex-col"
+                prefetch="render"
+              >
+                <TrophyIcon className="h-8 w-8 cursor-pointer mb-1y" />
+                <div className="cursor-pointer">My</div>
+                <div className="cursor-pointer text-xs">(admins)</div>
               </Link>
             </div>
-          }
-
-          {currentUser?.role === 'ADMIN' &&
-            <>
-              <div className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/challenges/mine' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}>
-                <Link to="/challenges/mine" className='flex items-center flex-col' prefetch='render'>
-                  <TrophyIcon className='h-8 w-8 cursor-pointer mb-1y' />
-                  <div className="cursor-pointer">My</div>
-                  <div className="cursor-pointer text-xs">(admins)</div>
-                </Link>
-              </div>
-              <div className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/challenges/all' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}>
-                <Link to="/challenges/all" className='flex items-center flex-col' prefetch='render'>
-                  <TrophyIcon className='h-8 w-8 cursor-pointer mb-1y' />
-                  <div className="cursor-pointer">All</div>
-                  <div className="cursor-pointer text-xs">(admins)</div>
-                </Link>
-              </div>
-              <div className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/admin' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}>
-                <Link to="/admin" className='flex items-center flex-col' prefetch='render'>
-                  <UsersIcon className='h-8 w-8 cursor-pointer mb-1y' />
-                  <div className="cursor-pointer">Users</div>
-                </Link>
-              </div>
-
-            </>
-          }
-          {/* <div className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mb-4 p-2 rounded-lg ${location.pathname === '/community' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}>
+            <div
+              className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/challenges/all' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}
+            >
+              <Link
+                to="/challenges/all"
+                className="flex items-center flex-col"
+                prefetch="render"
+              >
+                <TrophyIcon className="h-8 w-8 cursor-pointer mb-1y" />
+                <div className="cursor-pointer">All</div>
+                <div className="cursor-pointer text-xs">(admins)</div>
+              </Link>
+            </div>
+            <div
+              className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mt-4 mb-4 p-2 rounded-lg ${location.pathname === '/admin' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}
+            >
+              <Link
+                to="/admin"
+                className="flex items-center flex-col"
+                prefetch="render"
+              >
+                <UsersIcon className="h-8 w-8 cursor-pointer mb-1y" />
+                <div className="cursor-pointer">Users</div>
+              </Link>
+            </div>
+          </>
+        )}
+        {/* <div className={`w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mb-4 p-2 rounded-lg ${location.pathname === '/community' ? 'bg-gray-100' : 'hover:bg-gray-300'}`}>
             <Link to="/community" className='flex items-center flex-col'>
               <UsersIcon className='h-8 w-8 cursor-pointer mb-1' />
               <span className="cursor-pointer">Community</span>
@@ -134,16 +180,18 @@ const NavLinks = (): JSX.Element => {
               </Button>
             </Form>
           </div> */}
-           {currentUser &&
-          <div className='absolute bottom-10 left-0 w-24 h-20 text-darkgrey text-center mb-4 rounded-lg hover:bg-gray-300 flex flex-col items-center justify-center'>
-            <HiOutlineLogout className='h-8 w-8 cursor-pointer mb-1 text-darkgrey' onClick={logout}/>
-              <div className="cursor-pointer">Logout</div>
-            </div>
-          }
-          {navigation.state === 'loading' && <Spinner />}
-        </div>
-
-      </>
+        {currentUser && (
+          <div className="absolute bottom-10 left-0 w-24 h-20 text-darkgrey text-center mb-4 rounded-lg hover:bg-gray-300 flex flex-col items-center justify-center">
+            <HiOutlineLogout
+              className="h-8 w-8 cursor-pointer mb-1 text-darkgrey"
+              onClick={logout}
+            />
+            <div className="cursor-pointer">Logout</div>
+          </div>
+        )}
+        {navigation.state === 'loading' && <Spinner />}
+      </div>
+    </>
   )
 }
 
